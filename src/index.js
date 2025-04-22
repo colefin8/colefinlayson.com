c103964d9fd3cbcaa1225a96f10f5060ba9741fa
Cole Finlayson <cole.fin8@gmail.com>
2025-04-21 21:01:54 -0600

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ const links = {
     'AI': {
         'Digital Tar Pits': 'https://www.youtube.com/watch?v=vC2mlCtuJiU&pp=ygUMa3lsZSBoaWxsIGFp',
         'Tracing the Thoughts of a Large Language Model': 'https://www.anthropic.com/news/tracing-thoughts-language-model',
+        'The Art of Poison-Pilling Music Files': 'https://www.youtube.com/watch?v=xMYm2d9bmEA',
     },
     'MTG': {
         'Scryfall': 'https://www.scryfall.com',
         'Archidekt': 'https://archidekt.com/folders/655752',
         'EDHRec': 'https://edhrec.com/',
+    },
+    'Desktop Links': {
+        'YouTube Music': 'https://music.youtube.com/',
+        'YouTube': 'https://youtube.com',
+        'Gmail': 'https://mail.google.com/mail/u/0/#inbox',
+        'Netlify': 'https://app.netlify.com/teams/colefin8/sites',
     }
 }
 
