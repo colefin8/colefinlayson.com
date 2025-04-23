c0df610b37f96c18d0d43864d44e0dbde1915420
Cole Finlayson <cole.fin8@gmail.com>
2025-04-22 22:49:26 -0600

    init server branch

---
+++ nginx/src/index.js
@@ -0,0 +1,46 @@
+async function getLinks() {
+    try {
+        console.log('fetchin\'')
+        const response = await fetch('/links', {
+            method: 'GET'
+        })
+        if (!response.ok) {
+            throw new Error(`Error: response status ${response.status}`)
+        }
+
+        const json = await response.json();
+        populateLinks(json) 
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+function populateLinks(links) {
+    const linksDiv = document.getElementById('links')
+    for (let linkCategory in links) {
+        const header = document.createElement('h3')
+        const text = document.createTextNode(linkCategory)
+        const ul = document.createElement('ul')
+        
+        header.appendChild(text)
+        ul.classList.add('links-ul')
+
+        linksDiv.appendChild(header)
+        linksDiv.appendChild(ul)
+
+        for (let link in links[linkCategory]) {
+            const li = document.createElement('li')
+            const anchor = document.createElement('a')
+            const text = document.createTextNode(link)
+
+            anchor.setAttribute('href', links[linkCategory][link])
+            li.classList.add('link-li')
+            anchor.appendChild(text)
+            
+            li.appendChild(anchor)
+            ul.appendChild(li)
+        }
+    }
+}
+
+getLinks()
--- server/server.mjs
+++ server/server.mjs
@@ -1,22 +1,36 @@
 import http from 'http'
 import url from 'url'
-const PORT = process.env.PORT
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename)
 
 const links = {
     'AI': {
         'Digital Tar Pits': 'https://www.youtube.com/watch?v=vC2mlCtuJiU&pp=ygUMa3lsZSBoaWxsIGFp',
         'Tracing the Thoughts of a Large Language Model': 'https://www.anthropic.com/news/tracing-thoughts-language-model',
+        'The Art of Poison-Pilling Music Files': 'https://www.youtube.com/watch?v=xMYm2d9bmEA',
     },
     'MTG': {
         'Scryfall': 'https://www.scryfall.com',
         'Archidekt': 'https://archidekt.com/folders/655752',
         'EDHRec': 'https://edhrec.com/',
+    },
+    'Desktop Links': {
+        'YouTube Music': 'https://music.youtube.com/',
+        'YouTube': 'https://youtube.com',
+        'Gmail': 'https://mail.google.com/mail/u/0/#inbox',
+        'Netlify': 'https://app.netlify.com/teams/colefin8/sites',
+    },
+    'Cool Shit': {
+        'Fast Inverse Square Root - Quake III': 'https://www.youtube.com/watch?v=p8u_k2LIZyo',
     }
 }
 
 const server = http.createServer((req, res) => {
     const headers = {
-        'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
         'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
         'Access-Control-Max-Age': 2592000, // 30 days
     }
@@ -28,8 +42,13 @@ const server = http.createServer((req, res) => {
     }
 
     if (['GET', 'POST'].indexOf(req.method) > -1) {
-        console.log(url.parse(req.url))
+        console.log(req.url)
         switch (url.parse(req.url, true).pathname) {
+            case '/home':
+            case '/':
+                res.writeHead(200, headers);
+                fs.createReadStream('./pages/index.html').pipe(res)
+                return;
             case '/links':
                 res.writeHead(200, headers);
                 res.end(JSON.stringify(links))
@@ -43,6 +62,6 @@ const server = http.createServer((req, res) => {
 
 })
 
-server.listen(PORT, () => {
-    console.log(`server running on ${PORT}`)
+server.listen(3000, () => {
+    console.log(`server running on ${3000}`)
 })
